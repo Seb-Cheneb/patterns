00d7985d30068f77a8284a2034c968ab148caf9d
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-05-18 21:01:46 +0300

    debugger update

--- design_patterns/state.gd
+++ design_patterns/state.gd
@@ -2,31 +2,34 @@
 class_name State extends Node
 
 signal transition 
-static var player: Player
+
+@export var player: Player
+
+var owner_class = get_script().get_path().get_file().get_basename()
 
 
 func _ready() -> void:
-	push_warning("State :: _ready -- method not implemented")
+	push_warning("%s :: _ready -- method not implemented" % owner_class)
 
 
 func enter() -> void:
-	push_warning("State :: enter -- method not implemented")
+	push_warning("%s :: enter -- method not implemented" % owner_class)
 
 
 func exit() -> void:
-	printerr("State :: exit -- method not implemented")
+	push_warning("%s :: exit -- method not implemented" % owner_class)
 	
 	
 func process(_delta : float) -> State:
-	push_warning("State :: process -- method not implemented")
+	push_warning("%s :: process -- method not implemented" % owner_class)
 	return null
 
 
 func physics_process(_delta : float) -> State:
-	push_warning("State :: physics_process -- method not implemented")
+	push_warning("%s :: physics_process -- method not implemented" % owner_class)
 	return null
 	
 
 func unhandled_input(event: InputEvent) -> State:
-	push_warning("State :: unhandled_input -- method not implemented")
+	push_warning("%s :: unhandled_input -- method not implemented" % owner_class)
 	return null
