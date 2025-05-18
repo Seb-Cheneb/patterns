0bf9979c6c2f9b7a889728412e7d9bfe282414ed
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-05-18 20:50:36 +0300

    update

--- design_patterns/state.gd
+++ design_patterns/state.gd
@@ -6,28 +6,27 @@ static var player: Player
 
 
 func _ready() -> void:
-	printerr("method not implemented")
+	push_warning("State :: _ready -- method not implemented")
 
 
 func enter() -> void:
-	printerr("method not implemented")
+	push_warning("State :: enter -- method not implemented")
+
+
+func exit() -> void:
+	printerr("State :: exit -- method not implemented")
 	
 	
 func process(_delta : float) -> State:
+	push_warning("State :: process -- method not implemented")
 	return null
 
 
 func physics_process(_delta : float) -> State:
+	push_warning("State :: physics_process -- method not implemented")
 	return null
 	
 
 func unhandled_input(event: InputEvent) -> State:
+	push_warning("State :: unhandled_input -- method not implemented")
 	return null
-	
-
-#func next_transition() -> void:
-	#printerr("method not implemented")
-
-
-func exit() -> void:
-	printerr("method not implemented")
