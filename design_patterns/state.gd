df5312be48235cd4f7d687a01f7c98a75338607f
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-15 22:13:05 +0300

    Update submodule: scripts/patterns

--- design_patterns/state.gd
+++ design_patterns/state.gd
@@ -5,12 +5,10 @@ extends Node
 
 signal change_state_signal
 
-@export 
-var actor: Node3D
-
 @export_category("Debugging")
 @export 
 var is_debugging: bool = false
+
 
 func _ready() -> void:
 	# States start disabled - only the current state will be enabled
@@ -20,10 +18,10 @@ func _ready() -> void:
 func enter() -> void:
 	# Enable processing when entering the state
 	process_mode = Node.PROCESS_MODE_INHERIT
-	Logger.info(is_debugging, self, "entered state: " + name)
+	Logger.info(is_debugging, self, "entered state \"" + name + "\"")
 
 
 func exit() -> void:
 	# Disable processing when exiting the state
 	process_mode = Node.PROCESS_MODE_DISABLED
-	Logger.info(is_debugging, self, "exited state: " + name)
+	Logger.info(is_debugging, self, "exited state \"" + name + "\"")
--- design_patterns/state_machine.gd
+++ design_patterns/state_machine.gd
@@ -4,20 +4,15 @@ extends Node
 
 
 @export var initial_state: State
-
 @export_category("Debugging")
-@export 
-var is_debugging: bool = false
+@export var is_debugging: bool = false
 
 var states: Dictionary = {}
 var current_state: State
 var current_state_name: String
-var previous_state_name: String
 
 
 func _ready() -> void:
-	process_mode = Node.PROCESS_MODE_DISABLED
-	
 	for child in get_children():
 		if child is State:
 			child.change_state_signal.connect(change_state)
@@ -28,39 +23,27 @@ func _ready() -> void:
 		current_state = initial_state
 		current_state.name = initial_state.name.to_lower()
 		current_state.enter()
-		process_mode = Node.PROCESS_MODE_INHERIT
 	else:
 		Logger.warn(is_debugging, self, "no initial state set")
 
-
-func _process(delta: float) -> void:
-	current_state.on_process(delta)
-
-
-func _physics_process(delta: float) -> void:
-	current_state.on_physics_process(delta)
-	
-	
-func _unhandled_input(event: InputEvent) -> void:
-	current_state.on_unhandled_input(event)
-	
 	
 func change_state(new_state_name: String) -> void:
 	# sanity check
 	new_state_name = new_state_name.to_lower()
 	
 	if current_state_name == new_state_name:
-		Logger.warn(is_debugging, self, "trying to change the state to the same state")
+		Logger.warn(is_debugging, self, "trying to change to the same state")
 		return
 		
 	var new_state: State = states[new_state_name]
 	
 	if not new_state:
-		Logger.warn(is_debugging, self, "could not change to the state named \"" + new_state_name +"\"")
+		Logger.warn(is_debugging, self, "could not find state \"" + new_state_name +"\"")
 		return
+	
+	if current_state:
+		current_state.exit()
 		
-	current_state.exit()
-	new_state.enter()
-	previous_state_name = current_state.name
 	current_state = new_state
 	current_state_name = new_state_name
+	current_state.enter()
